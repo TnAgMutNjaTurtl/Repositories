6d32a2bd3e0a54f65f637093ab185a9eb8e7c26b
TnAgMutNjaTurtl <joseph.d.pope@gmail.com>
2020-02-25 23:11:32 -0600

    all letters ascii art or ascii art osu if wrong input

--- ascii.cpp
+++ ascii.cpp
@@ -1,18 +1,310 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-
-cout << "   *******    ******** **     **" << endl;
-cout << "  **/////**  **////// /**    /**" << endl;
-cout << " **     //**/**       /**    /**" << endl;
-cout << "/**      /**/*********/**    /**" << endl;
-cout << "/**      /**////////**/**    /**" << endl;
-cout << "//**     **        /**/**    /**" << endl;
-cout << " //*******   ******** //******* " << endl;
-cout << "  ///////   ////////   ///////  " << endl;
-
-return 0;
-
+    int value = 0;
+    char letter(1);
+    cout << "Enter one letter to see the ascii art and letter value \n"
+    << "Enter a word, number or spec character to end program:  ";
+    cin >> letter;
+    switch (letter)
+    {
+        case 'A':
+            cout << "     ** \n";   
+            cout << "    **** \n";  
+            cout << "   **//** \n";
+            cout << "  **  //** \n";
+            cout << " ********** \n";
+            cout << "/**//////** \n";
+            cout << "/**     /** \n";
+            cout << "//      // \n";
+            value = (value + 1);
+            break;
+        case 'B':
+            cout << "  ****** \n";
+            cout << " /*////** \n";
+            cout << " /*   /** \n";
+            cout << " /****** \n";
+            cout << " /*//// ** \n";
+            cout << " /*    /** \n";
+            cout << " /******* \n";
+            cout << " /////// \n";
+            value = (value + 2);
+            break;
+        case 'C':
+            cout << "   ******  \n";
+            cout << "  **////** \n";
+            cout << " **    //  \n";
+            cout << "/**        \n";
+            cout << "/**        \n";
+            cout << "//**    ** \n";
+            cout << " //******  \n";
+            cout << "  //////   \n";
+            value = (value + 2);
+            break;
+        case 'D':
+            cout << "  *******      \n";
+            cout << " /**////**     \n";
+            cout << " /**    /**    \n";
+            cout << " /**    /**    \n";
+            cout << " /**    /**    \n";
+            cout << " /**    **     \n";
+            cout << " /*******      \n";
+            cout << " ///////       \n";
+            value = (value + 2);
+            break;
+        case 'E':
+            cout << "  ********     \n";
+            cout << " /**/////      \n";
+            cout << " /**           \n";
+            cout << " /*******      \n";
+            cout << " /**////       \n";
+            cout << " /**           \n";
+            cout << " /********     \n";
+            cout << " ////////      \n";
+            value = (value + 2);
+            break;
+        case 'F':
+            cout << "  ********     \n";
+            cout << " /**/////      \n";
+            cout << " /**           \n";
+            cout << " /*******      \n";
+            cout << " /**////       \n";
+            cout << " /**           \n";
+            cout << " /**           \n";
+            cout << " //            \n";
+            value = (value + 2);
+            break;
+        case 'G':
+            cout << "    ********   \n";
+            cout << "   **//////**  \n";
+            cout << "  **      //   \n";
+            cout << " /**           \n";
+            cout << " /**    *****  \n";
+            cout << " //**  ////**  \n";
+            cout << "  //********   \n";
+            cout << "   ////////    \n";
+            value = (value + 2);
+            break;
+        case 'H':
+            cout << "  **      **   \n";
+            cout << " /**     /**   \n";
+            cout << " /**     /**   \n";
+            cout << " /**********   \n";
+            cout << " /**//////**   \n";
+            cout << " /**     /**   \n";
+            cout << " /**     /**   \n";
+            cout << " //      //    \n";
+            value = (value + 2);
+            break;
+        case 'I':
+            cout << "  **           \n";
+            cout << " /**           \n";
+            cout << " /**           \n";
+            cout << " /**           \n";
+            cout << " /**           \n";
+            cout << " /**           \n";
+            cout << " /**           \n";
+            cout << " //            \n";
+            value = (value + 2);
+            break;
+        case 'J':
+            cout << "       **      \n";
+            cout << "      /**      \n";
+            cout << "      /**      \n";
+            cout << "      /**      \n";
+            cout << "      /**      \n";
+            cout << "  **  /**      \n";
+            cout << " //*****       \n";
+            cout << "  /////        \n";
+            value = (value + 2);
+            break;
+        case 'K':
+            cout << "  **   **      \n";
+            cout << " /**  **       \n";
+            cout << " /** **        \n";
+            cout << " /****         \n";
+            cout << " /**/**        \n";
+            cout << " /**//**       \n";
+            cout << " /** //**      \n";
+            cout << " //   //       \n";
+            value = (value + 2);
+            break;
+        case 'L':
+            cout << "  **           \n";
+            cout << " /**           \n";
+            cout << " /**           \n";
+            cout << " /**           \n";
+            cout << " /**           \n";
+            cout << " /**           \n";
+            cout << " /********     \n";
+            cout << " ////////      \n";
+            value = (value + 2);
+            break;
+        case 'M':
+            cout << "  ****     ****\n";
+            cout << " /**/**   **/**\n";
+            cout << " /**//** ** /**\n";
+            cout << " /** //***  /**\n";
+            cout << " /**  //*   /**\n";
+            cout << " /**   /    /**\n";
+            cout << " /**        /**\n";
+            cout << " //         // \n";
+            value = (value + 2);
+            break;
+        case 'N':
+            cout << "  ****     **  \n";
+            cout << " /**/**   /**  \n";
+            cout << " /**//**  /**  \n";
+            cout << " /** //** /**  \n";
+            cout << " /**  //**/**  \n";
+            cout << " /**   //****  \n";
+            cout << " /**    //***  \n";
+            cout << " //      ///   \n";
+            value = (value + 2);
+            break;
+        case 'O':
+            cout << "    *******    \n";
+            cout << "   **/////**   \n";
+            cout << "  **     //**  \n";
+            cout << " /**      /**  \n";
+            cout << " /**      /**  \n";
+            cout << " //**     **   \n";
+            cout << "  //*******    \n";
+            cout << "   ///////     \n";
+            value = (value + 2);
+            break;
+        case 'P':
+            cout << "  *******      \n";
+            cout << " /**////**     \n";
+            cout << " /**   /**     \n";
+            cout << " /*******      \n";
+            cout << " /**////       \n";
+            cout << " /**           \n";
+            cout << " /**           \n";
+            cout << " //            \n";
+            value = (value + 2);
+            break;
+        case 'Q':
+            cout << "    *******    \n";
+            cout << "   **/////**   \n";
+            cout << "  **     //**  \n";
+            cout << " /**      /**  \n";
+            cout << " /**    **/**  \n";
+            cout << " //**  // **   \n";
+            cout << "  //******* ** \n";
+            cout << "   /////// //  \n";
+            value = (value + 2);
+            break;
+        case 'R':
+            cout << "  *******      \n";
+            cout << " /**////**     \n";
+            cout << " /**   /**     \n";
+            cout << " /*******      \n";
+            cout << " /**///**      \n";
+            cout << " /**  //**     \n";
+            cout << " /**   //**    \n";
+            cout << " //     //     \n";
+            value = (value + 2);
+            break;
+        case 'S':
+            cout << "   ********    \n";
+            cout << "  **//////     \n";
+            cout << " /**           \n";
+            cout << " /*********    \n";
+            cout << " ////////**    \n";
+            cout << "        /**    \n";
+            cout << "  ********     \n";
+            cout << " ////////      \n";
+            value = (value + 2);
+            break;
+        case 'T':
+            cout << "  **********   \n";
+            cout << " /////**///    \n";
+            cout << "     /**       \n";
+            cout << "     /**       \n";
+            cout << "     /**       \n";
+            cout << "     /**       \n";
+            cout << "     /**       \n";
+            cout << "     //        \n";
+            value = (value + 2);
+            break;
+        case 'U':
+            cout << "  **     **    \n";
+            cout << " /**    /**    \n";
+            cout << " /**    /**    \n";
+            cout << " /**    /**    \n";
+            cout << " /**    /**    \n";
+            cout << " /**    /**    \n";
+            cout << " //*******     \n";
+            cout << "  ///////      \n";
+            value = (value + 21);
+            break;
+        case 'V':
+            cout << "  **      **   \n";
+            cout << " /**     /**   \n";
+            cout << " /**     /**   \n";
+            cout << " //**    **    \n";
+            cout << "  //**  **     \n";
+            cout << "   //****      \n";
+            cout << "    //**       \n";
+            cout << "     //        \n";
+            value = (value + 22);
+            break;
+        case 'W':
+            cout << "  **       **  \n";
+            cout << " /**      /**  \n";
+            cout << " /**   *  /**  \n";
+            cout << " /**  *** /**  \n";
+            cout << " /** **/**/**  \n";
+            cout << " /**** //****  \n";
+            cout << " /**/   ///**  \n";
+            cout << " //       //   \n";
+            value = (value + 23);
+            break;
+        case 'X':
+            cout << "  **     **    \n";
+            cout << " //**   **     \n";
+            cout << "  //** **      \n";
+            cout << "   //***       \n";
+            cout << "    **/**      \n";
+            cout << "   ** //**     \n";
+            cout << "  **   //**    \n";
+            cout << " //     //     \n";
+            value = (value + 24);
+            break;
+        case 'Y':
+            cout << "  **    **     \n";
+            cout << " //**  **      \n";
+            cout << "  //****       \n";
+            cout << "   //**        \n";
+            cout << "    /**        \n";
+            cout << "    /**        \n";
+            cout << "    /**        \n";
+            cout << "    //         \n";
+            value = (value + 25);
+            break;
+        case 'Z':
+            cout << "  ********     \n";
+            cout << " //////**      \n";
+            cout << "      **       \n";
+            cout << "     **        \n";
+            cout << "    **         \n";
+            cout << "   **          \n";
+            cout << "  ********     \n";
+            cout << " ////////      \n";
+            value = (value + 26);
+            break;    
+        default:
+            cout << "    *******     ********  **     ** \n";
+            cout << "   **/////**   **//////  /**    /** \n";
+            cout << "  **     //** /**        /**    /** \n";
+            cout << " /**      /** /********* /**    /** \n";
+            cout << " /**      /** ////////** /**    /** \n";
+            cout << " //**     **         /** /**    /** \n";
+            cout << "  //*******    ********  //*******  \n";
+            cout << "   ///////    ////////    ///////   \n";
+    }
+    cout << "End of program. \n";
+    return value;
 }
